ff8b964f891af74d5338a0c0bf45a5700f671183
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-03-25 13:49:06 +0530

    Time: 91 ms (58.05%), Space: 33.1 MB (47.56%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        //vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
         vector<vector<int>> dis(n,vector<int>(m,-1));
         vector<int> dir={-1,0,+1,0,-1};
         queue<pair<int,int>> q;
@@ -11,6 +11,7 @@ public:
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     dis[i][j]=0;
+                    vis[i][j]=1;
                     q.push({i,j});
                 }
             }
@@ -22,9 +23,10 @@ public:
             for(int i=0;i<4;i++){
                 int nr=r+dir[i];
                 int nc=c+dir[i+1];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && dis[nr][nc]==-1){
+                if(nr>=0 && nr<n && nc>=0 && nc<m && dis[nr][nc]==-1 && !vis[nr][nc]){
                     q.push({nr,nc});
                     dis[nr][nc]=dis[r][c]+1;
+                    vis[nr][nc]=1;
                 }
             }
         }
