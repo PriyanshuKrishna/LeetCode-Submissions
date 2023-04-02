9f5de1896c923477373ec2fec3f1a9f60c45fc22
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-02 12:59:56 +0530

    Time: 26 ms (12.50%), Space: 29.8 MB (12.50%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        vector<vector<int>> arr;
-        map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-            mp[nums[i]]++;
-        queue<pair<int,int>> q;
-        for(auto it:mp){
-            q.push({it.first,it.second});
-        }
-        while(!q.empty()){
-            int sz=q.size();
+        vector<vector<int>> res;
+        unordered_map<int,int> mp;
+        for(auto it:nums)
+            mp[it]++;
+        while(mp.size()){
             vector<int> temp;
-            for(int i=0;i<sz;i++){
-                auto node=q.front();q.pop();
-                int num=node.first;
-                int freq=node.second;
-                temp.push_back(num);
-                --freq;
-                if(freq>0)
-                    q.push({num,freq});
-            }
-            arr.push_back(temp);
+            for(auto it:mp)
+                temp.push_back(it.first);
+            for(auto it:temp)
+                if(!(--mp[it]))
+                    mp.erase(it);
+            res.push_back(temp);
         }
-        return arr;
+        return res;
     }
 };
