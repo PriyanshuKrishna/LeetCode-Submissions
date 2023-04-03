56539f05086b0f2f47d2fb314caa313981114a9c
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-04 03:55:21 +0530

    Time: 33 ms (47.35%), Space: 15.9 MB (95.35%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -10,36 +10,19 @@
  * };
  */
 class Solution {
-private:
-    void dfs(TreeNode* root,long long target,int& count){
-        if(root==NULL)  return;
-        if(root->val==target)
-            count++;
-        dfs(root->left,target-root->val,count);
-        dfs(root->right,target-root->val,count);
-    }
 public:
+    int count=0;
+    void dfs(TreeNode* root,long long target){
+        if(root==NULL)  return;
+        if(root->val==target)  count++;
+        dfs(root->left,target-root->val);
+        dfs(root->right,target-root->val);
+    }
     int pathSum(TreeNode* root, int targetSum) {
-        int count=0;
-        vector<vector<TreeNode*>> res;
-        if(root==NULL) 
-            return count;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int sz=q.size();
-            vector<TreeNode*> temp;
-            for(int i=0;i<sz;i++){
-                TreeNode* node=q.front();q.pop();
-                temp.push_back(node);
-                if(node->left)  q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            res.push_back(temp);
-        }
-        for(auto i:res){
-            for(auto it:i)
-                dfs(it,(long long)targetSum,count);
+        if(root){
+            dfs(root,(long long)targetSum);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
         }
         return count;
     }
