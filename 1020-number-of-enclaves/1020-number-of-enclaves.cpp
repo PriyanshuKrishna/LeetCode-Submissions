a232c73d8b11b7a27207bab24c2e95f156abd037
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-07 10:01:22 +0530

    Time: 104 ms (13.48%), Space: 54.2 MB (5.56%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,43 +1,34 @@
 class Solution {
 private:
-    void dfs(int row,int col,vector<vector<int>>& grid,vector<vector<int>>& vis){
-        int n=grid.size();
-        int m=grid[0].size();
-        vis[row][col]=1;
+    void dfs(vector<vector<int>>& grid,int r,int c){
+        grid[r][c]=0;
         vector<int> dir={-1,0,+1,0,-1};
         for(int i=0;i<4;i++){
-            int nr=row+dir[i];
-            int nc=col+dir[i+1];
-            if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==1)
-                dfs(nr,nc,grid,vis);
+            int nr=r+dir[i];
+            int nc=c+dir[i+1];
+            if(nr>=0 && nr<grid.size() && nc>=0 && nc<grid[0].size()
+                        && grid[nr][nc]==1)
+                dfs(grid,nr,nc);
         }
     }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        int count=0;
-        for(int j=0;j<m;j++){
-            if(!vis[0][j] && grid[0][j]==1)
-                dfs(0,j,grid,vis);
-            if(!vis[n-1][j] && grid[n-1][j]==1)
-                dfs(n-1,j,grid,vis);
-        }
-        for(int i=0;i<n;i++){
-            if(!vis[i][0] && grid[i][0]==1)
-                dfs(i,0,grid,vis);
-            if(!vis[i][m-1] && grid[i][m-1]==1)
-                dfs(i,m-1,grid,vis);
-        }
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]==1)
+                if((i==0 || j==0 || j==m-1 || i==n-1) && grid[i][j]==1) 
+                    dfs(grid,i,j);
+            }
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)
                     count++;
             }
         }
         return count;
     }
 };
-
