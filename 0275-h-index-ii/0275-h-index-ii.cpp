9e29707443ccca9145318bd9bb4483beba9a92a6
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-14 00:49:50 +0530

    Time: 25 ms (21.61%), Space: 18.7 MB (55.36%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n=citations.size(),ans=0;
-        sort(citations.begin(),citations.end());
         for(int i=0;i<n;i++){
             if(citations[i]>=n-i)
                 return n-i;
