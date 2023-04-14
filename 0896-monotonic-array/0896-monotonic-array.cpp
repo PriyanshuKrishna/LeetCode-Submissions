2c925b16f62a00ec0dc4382d8059c1bde0082511
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-15 01:45:11 +0530

    Time: 162 ms (86.96%), Space: 96.5 MB (74.18%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool incr=true,decr=true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1])
+                incr=false;
+            if(nums[i]<nums[i+1])
+                decr=false;
+        }
+        return incr||decr;
+    }
+};
