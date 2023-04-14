1f46cda514a3512e374d7dc32a76d7ecc30e7b9d
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-15 01:13:10 +0530

    Time: 254 ms (26.67%), Space: 74.1 MB (33.98%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> res(n,0);
-        for(int i=0;i<bookings.size();i++){
-            res[bookings[i][0]-1]+=bookings[i][2];
-            if(bookings[i][1]<n)
-                res[bookings[i][1]]-=bookings[i][2];
+        for(auto it:bookings){
+            res[it[0]-1]+=it[2];
+            if(it[1]<n)
+                res[it[1]]-=it[2];
         }
         for(int i=1;i<n;i++)
             res[i]+=res[i-1];
