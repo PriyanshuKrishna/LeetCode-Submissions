2491b8d5190db04941f0dce2c87bcb6c4491c376
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-04-15 22:17:35 +0530

    Time: 693 ms (75.00%), Space: 299.6 MB (50.00%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -13,9 +13,9 @@ class Solution {
 public:
     TreeNode* replaceValueInTree(TreeNode* root) {
         int level=0;
-        unordered_map<int,int> mp;
         queue<TreeNode*> q;
         q.push(root);
+        unordered_map<int,int> mp;
         mp[0]=root->val;
         while(!q.empty()){
             level++;
@@ -24,24 +24,25 @@ public:
                 TreeNode* node=q.front();q.pop();
                 if(node->left){
                     q.push(node->left);
-                    mp[level]+=(node->left->val);
+                    mp[level]+=node->left->val;
                 }
                 if(node->right){
                     q.push(node->right);
-                    mp[level]+=(node->right->val);
+                    mp[level]+=node->right->val;
                 }
             }
         }
-        q.push(root);
         level=0;
+        q.push(root);
         root->val=0;
         while(!q.empty()){
             level++;
             int n=q.size();
+            
             for(int i=0;i<n;i++){
                 TreeNode* node=q.front();q.pop();
-                int left=(node->left)?(node->left->val):0;
-                int right=(node->right)?(node->right->val):0;
+                int left=node->left?(node->left->val):0;
+                int right=node->right?(node->right->val):0;
                 if(node->left){
                     q.push(node->left);
                     node->left->val=mp[level]-left-right;
