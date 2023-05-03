aebf12ddb0433fd8f855939f225aa181c908cf6c
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-05-04 04:07:29 +0530

    Time: 52 ms (82.11%), Space: 31.8 MB (65.12%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> res;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        set<int> n1,n2;
-        for(int i=0;i<nums1.size();i++){
-            n1.insert(nums1[i]);
+        unordered_set<int> n1(nums1.begin(),nums1.end());
+        unordered_set<int> n2(nums2.begin(),nums2.end());
+        vector<int> rs1,rs2;
+        for(auto it:n1){
+            if(!n2.count(it))
+                rs1.push_back(it);
         }
-        for(int i=0;i<nums2.size();i++)
-            n2.insert(nums2[i]);
-        vector<int> tmp;
-        for(int i=0;i<nums1.size();i++){
-            if(i==0){
-                if(!n2.count(nums1[i]))
-                    tmp.push_back(nums1[i]);
-            }
-            else if(i>0 && nums1[i]!=nums1[i-1]){
-                if(!n2.count(nums1[i]))
-                    tmp.push_back(nums1[i]);
-            }
+        for(auto it:n2){
+            if(!n1.count(it))
+                rs2.push_back(it);
         }
-        res.push_back(tmp);
-        vector<int> temp;
-        for(int i=0;i<nums2.size();i++){
-            if(i==0){
-                if(!n1.count(nums2[i]))
-                    temp.push_back(nums2[i]);
-            }
-            else if(i>0 && nums2[i]!=nums2[i-1]){
-                if(!n1.count(nums2[i]))
-                    temp.push_back(nums2[i]);
-            }
-        }
-        res.push_back(temp);
-        return res;
+        return {rs1,rs2};
     }
 };
