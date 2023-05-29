5a581da59e55e0a6a8979cbe4ecb979b7fd4654e
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-05-30 04:35:15 +0530

    Time: 17 ms (74.74%), Space: 12.3 MB (69.86%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int n=nums.size(),count=0;
+        int n=target.size(),count=0;
+        unordered_map<string,int> mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                string tmp=(nums[i]+nums[j]);
-                if(i!=j && tmp==target) 
-                    count++;
-            }
+            string s1=target.substr(0,i);
+            string s2=target.substr(i,n);
+            if(s1==s2)
+                count+=mp[s1]*(mp[s2]-1);
+            else
+                count+=mp[s1]*mp[s2];
         }
         return count;
     }
