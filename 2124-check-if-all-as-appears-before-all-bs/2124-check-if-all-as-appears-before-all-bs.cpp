2fb749dabcbb775ba2b2646244c35d962d6d0d27
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-05-31 21:52:35 +0530

    Time: 2 ms (46.03%), Space: 6.2 MB (58.96%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool checkString(string s) {
-        vector<int> tmp(2,0);
-        tmp[1]=INT_MAX;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='a')
-                tmp[0]=max(tmp[0],i);
-            else
-                tmp[1]=min(tmp[1],i);
-        }
-        return tmp[0]<=tmp[1]?true:false;
+        return is_sorted(s.begin(),s.end());
     }
 };
