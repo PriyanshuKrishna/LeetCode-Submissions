a1bccb3216e7dde955e7311812367bb63921c24a
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-06-03 02:41:01 +0530

    Time: 35 ms (31.23%), Space: 27 MB (41.16%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -4,9 +4,7 @@ public:
         int count=0;
         for(int i=1;i<nums.size();i++){
             if(nums[i]<nums[i-1]){
-                if(count++)
-                    return false;
-                if(i>1 && i<nums.size()-1 && nums[i-2]>nums[i] && nums[i+1]<nums[i-1])
+                if(count++ || (i>1 && i<nums.size()-1 && nums[i-2]>nums[i] && nums[i+1]<nums[i-1]))
                     return false;
             }
         }
