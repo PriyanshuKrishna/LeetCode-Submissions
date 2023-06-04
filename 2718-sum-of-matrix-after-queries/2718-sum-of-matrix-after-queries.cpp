a18ae348adb1b48962e5956d74fdab1ac3301b77
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-06-04 09:58:05 +0530

    Time: 688 ms (9.09%), Space: 211.9 MB (9.09%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
@@ -3,31 +3,22 @@ public:
     long long matrixSumQueries(int n, vector<vector<int>>& queries) {
         long long sum=0;
         // vector<vector<int>> nums(n,vector<int>(n,0));
-        int sze=queries.size();
-        unordered_set<int>strow;
-        unordered_set<int>stcol;
-        for(int i=sze-1;i>=0;i--){
+        int m=queries.size();
+        unordered_set<int>stcol,strow;
+        for(int i=m-1;i>=0;i--){
             long long type=queries[i][0];
             long long val=queries[i][2];
             long long ind=queries[i][1];
-            if(type==1){
-                 if(stcol.count(ind)!=0)
-                    continue;
-                else{
-                    sum+=(n-strow.size())*val;
-                    stcol.insert(ind);
-                }
+            if(type){
+                if(stcol.count(ind))  continue;
+                sum+=(n-strow.size())*val;
+                stcol.insert(ind);
+                continue;
             }
-            else{
-                if(strow.count(ind)!=0)
-                    continue;
-                else{
-                    sum+=(n-stcol.size())*val;
-                    strow.insert(ind);
-                }
-            }
+            if(strow.count(ind)) continue;
+            sum+=(n-stcol.size())*val;
+            strow.insert(ind);
         }
         return sum;
-        
     }
 };
