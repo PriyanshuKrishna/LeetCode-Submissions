79c09c81967a57184f5e8f65d63137826a412fb3
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-07-12 14:02:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.57%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int n=s.length(),absent=0;
+        int absent=0,late=0;
+        for(auto it:s){
+            if(it=='A') absent++;
+            if(absent>=2) return false;
+            if(it=='L') late++;
+            if(late>=3) return false;
+            if(it!='L') late=0;
+        }
+        return true;
+    }
+};
+/*
+int n=s.length(),absent=0;
         for(int i=0;i<n;i++){
             if(absent>=2) return false;
             if(s[i]=='A') absent++;
@@ -12,5 +24,5 @@ public:
          }
         if(absent>=2) return false;
         return true;
-    }
-};
+        
+*/
