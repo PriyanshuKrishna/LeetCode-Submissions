498c99fafd9a3f96305ba3ea5784bf3ca8bcb0cb
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-07-21 02:30:01 +0530

    Time: 32 ms (86.12%), Space: 71.2 MB (54.12%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,45 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* lst){
-        ListNode* currptr=lst;
+    ListNode* reverseLL(ListNode* head){
         ListNode* prevptr=NULL;
-        ListNode* nextptr=NULL;
+        ListNode* curptr=head;
+        ListNode* nextptr;
         
-        while(currptr!=NULL){
-            nextptr=currptr->next;
-            currptr->next=prevptr;
+        while(curptr){
+            nextptr=curptr->next;
+            curptr->next=prevptr;
             
-            prevptr=currptr;
-            currptr=nextptr;
+            prevptr=curptr;
+            curptr=nextptr;
         }
         return prevptr;
     }
-
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy=new ListNode(0);
-        ListNode* curr=dummy;
+        ListNode* dummy=new ListNode();
+        ListNode* cur=dummy;
         l1=reverseLL(l1);
         l2=reverseLL(l2);
         int carry=0;
-        while(l1!=NULL || l2!=NULL || carry==1){
+        while(l1 || l2 || carry){
             int sum=0;
-            if(l1!=NULL){
+            if(l1){
                 sum+=l1->val;
                 l1=l1->next;
             }
-            if(l2!=NULL){
+            if(l2){
                 sum+=l2->val;
                 l2=l2->next;
             }
             sum+=carry;
             carry=sum/10;
             ListNode* node=new ListNode(sum%10);
-            curr->next=node;
-            curr=curr->next;
+            cur->next=node;
+            cur=cur->next;
         }
-        l1=reverseLL(l1);
-        l2=reverseLL(l2);
         ListNode* res=reverseLL(dummy->next);
         return res;
     }
