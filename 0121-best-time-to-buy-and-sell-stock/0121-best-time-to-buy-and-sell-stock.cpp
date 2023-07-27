b10c84a3e02275a81132c04d39d802ddef74e500
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-07-27 21:53:09 +0530

    Time: 81 ms (99.91%), Space: 93.3 MB (29.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buyday=prices[0],sellday=0,res=0;
+        int buy=prices[0],sell=0,res=0;
         for(int i=1;i<prices.size();i++){
-            sellday=prices[i];
-            if((sellday-buyday)>0)
-                res=max(res,(sellday-buyday));
+            sell=prices[i];
+            if((sell-buy)>0)
+                res=max(res,(sell-buy));
             else
-                buyday=prices[i];
+                buy=min(buy,prices[i]);
         }
         return res;
     }
