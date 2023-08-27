b08698c21fb168140abd68478db936f7a02e1c04
PriyanshuKrishna <92053940+PriyanshuKrishna@users.noreply.github.com>
2023-08-27 18:57:21 +0530

    Time: 217 ms (14.29%), Space: 47.3 MB (14.29%) - LeetHub

--- 2834-find-the-minimum-possible-sum-of-a-beautiful-array/2834-find-the-minimum-possible-sum-of-a-beautiful-array.cpp
+++ 2834-find-the-minimum-possible-sum-of-a-beautiful-array/2834-find-the-minimum-possible-sum-of-a-beautiful-array.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    long long minimumPossibleSum(int n, int target) {
+    long long minimumPossibleSum(int n, int k) {
+        int ind=1,size=0;
+        long long sum=0;
         set<int> st;
-        long long ans=0;
-        for(int i=1;st.size()<n;i++){
-            if(st.find(target-i)==st.end()){
-                st.insert(i);
-                ans+=i;
+        while(size<n){
+            if(st.find(k-ind)!=st.end()){
+                ind++;
+                continue;
             }
+            sum+=ind;
+            st.insert(ind);
+            ind++;
+            size++;
         }
-        return ans;
+        return sum;
     }
 };
